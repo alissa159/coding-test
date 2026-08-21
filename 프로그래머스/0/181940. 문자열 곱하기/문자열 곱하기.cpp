204c7e8e8752a06f9b6439ce72b759a28d508d4e
윤서현 <seohyunyun325@gmail.com>
2026-08-21 22:17:36 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cpp
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 string solution(string my_string, int k) {
     string answer = "";
-    for(int i=0; i<k; i++){
+    
+    for (int i=0; i<k; i++){
         answer+= my_string;
     }
+    
     return answer;
 }
