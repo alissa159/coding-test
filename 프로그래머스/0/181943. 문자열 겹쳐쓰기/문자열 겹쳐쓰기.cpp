e0899a6ea0417dfd45ed84f6e3344c34e64be9fb
윤서현 <seohyunyun325@gmail.com>
2026-08-21 20:39:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -6,8 +6,6 @@ using namespace std;
 string solution(string my_string, string overwrite_string, int s) {
     string answer = "";
     
-    string front = my_string.substr(0, s);
-    string back = my_string.substr(s+ overwrite_string.size());
-    answer= front + overwrite_string + back;
+    answer = my_string.replace(s, overwrite_string.size(), overwrite_string);
     return answer;
 }
